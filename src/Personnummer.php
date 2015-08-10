6447477b71e91a724828f4ca3541ee8e725e545e
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2015-08-10 17:01:55 +0200

    Code style

--- src/Personnummer.php
+++ src/Personnummer.php
@@ -5,8 +5,8 @@ namespace Frozzare\Personnummer;
 use Datetime;
 use Exception;
 
-final class Personnummer {
-
+final class Personnummer
+{
   /**
    * The Luhn algorithm.
    *
@@ -14,22 +14,23 @@ final class Personnummer {
    *
    * @return int
    */
-  private static function luhn ($str) {
-	$v   = 0;
-	$sum = 0;
+  private static function luhn($str)
+  {
+      $v = 0;
+      $sum = 0;
 
-	for ($i = 0; $i < strlen($str); $i++) {
-	  $v = intval($str[$i]);
-	  $v *= 2 - ($i % 2);
+      for ($i = 0; $i < strlen($str); $i++) {
+          $v = intval($str[$i]);
+          $v *= 2 - ($i % 2);
 
-	  if ($v > 9) {
-		$v -= 9;
-	  }
+          if ($v > 9) {
+              $v -= 9;
+          }
 
-	  $sum += $v;
-	}
+          $sum += $v;
+      }
 
-	return intval(ceil($sum / 10) * 10 - $sum);
+      return intval(ceil($sum / 10) * 10 - $sum);
   }
 
   /**
@@ -41,23 +42,24 @@ final class Personnummer {
    *
    * @return bool
    */
-  private static function testDate($year, $month, $day) {
-	try {
-	  date_default_timezone_set('Europe/Stockholm');
-	  $date = new DateTime($year . '-' . $month . '-' . $day);
+  private static function testDate($year, $month, $day)
+  {
+      try {
+          date_default_timezone_set('Europe/Stockholm');
+          $date = new DateTime($year.'-'.$month.'-'.$day);
 
-	  if (strlen($month) < 2) {
-		$month = '0' . $month;
-	  }
+          if (strlen($month) < 2) {
+              $month = '0'.$month;
+          }
 
-	  if (strlen($day) < 2) {
-		$day = '0' . $day;
-	  }
+          if (strlen($day) < 2) {
+              $day = '0'.$day;
+          }
 
-	  return !(substr($date->format('Y'), 2) !== strval($year) || $date->format('m') !== strval($month) || $date->format('d') !== strval($day));
-	} catch (Exception $e) {
-	  return false;
-	}
+          return !(substr($date->format('Y'), 2) !== strval($year) || $date->format('m') !== strval($month) || $date->format('d') !== strval($day));
+      } catch (Exception $e) {
+          return false;
+      }
   }
 
   /**
@@ -67,39 +69,39 @@ final class Personnummer {
    *
    * @return bool
    */
-  public static function valid($str) {
-	if (!is_numeric($str) && !is_string($str)) {
-	  return false;
-	}
+  public static function valid($str)
+  {
+      if (!is_numeric($str) && !is_string($str)) {
+          return false;
+      }
 
-	$str = strval($str);
+      $str = strval($str);
 
-	$reg   = '/^(\d{2}){0,1}(\d{2})(\d{2})(\d{2})([\-|\+]{0,1})?(\d{3})(\d{0,1})$/';
-	preg_match($reg, $str, $match);
+      $reg = '/^(\d{2}){0,1}(\d{2})(\d{2})(\d{2})([\-|\+]{0,1})?(\d{3})(\d{0,1})$/';
+      preg_match($reg, $str, $match);
 
-	if (!isset($match) || count($match) < 7) {
-	  return false;
-	}
+      if (!isset($match) || count($match) < 7) {
+          return false;
+      }
 
-	$century = $match[1];
-	$year    = $match[2];
-	$month   = $match[3];
-	$day     = $match[4];
-	$sep     = $match[5];
-	$num     = $match[6];
-	$check   = $match[7];
+      $century = $match[1];
+      $year = $match[2];
+      $month = $match[3];
+      $day = $match[4];
+      $sep = $match[5];
+      $num = $match[6];
+      $check = $match[7];
 
-	if (strlen($year) === 4) {
-	  $year = substr($year, 2);
-	}
+      if (strlen($year) === 4) {
+          $year = substr($year, 2);
+      }
 
-	$valid = self::luhn($year . $month . $day . $num) === intval($check);
+      $valid = self::luhn($year.$month.$day.$num) === intval($check);
 
-	if ($valid && self::testDate($year, $month, $day)) {
-	  return $valid;
-	}
+      if ($valid && self::testDate($year, $month, $day)) {
+          return $valid;
+      }
 
-	return $valid && self::testDate($year, $month, (intval($day) - 60));
+      return $valid && self::testDate($year, $month, (intval($day) - 60));
   }
-
 }
